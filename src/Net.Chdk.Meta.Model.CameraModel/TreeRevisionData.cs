76d6d709864ac8ea4699d8cb0954c36d527f8960
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 06:03:53 +0300

    Add TreeRevisionData Add TreeSourceData

---
+++ src/Net.Chdk.Meta.Model.CameraModel/TreeRevisionData.cs
@@ -0,0 +1,7 @@
+﻿namespace Net.Chdk.Meta.Model.CameraModel
+{
+    public sealed class TreeRevisionData
+    {
+        public TreeSourceData Source { get; set; }
+    }
+}
---
+++ src/Net.Chdk.Meta.Model.CameraModel/TreeSourceData.cs
@@ -0,0 +1,8 @@
+﻿namespace Net.Chdk.Meta.Model.CameraModel
+{
+    public sealed class TreeSourceData
+    {
+        public string Platform { get; set; }
+        public string Revision { get; set; }
+    }
+}
