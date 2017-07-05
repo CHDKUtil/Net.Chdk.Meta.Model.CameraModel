d78e5831a385c960f39779860465d123b2c5a1de
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 06:28:41 +0300

    Add TreePlatformData

---
+++ src/Net.Chdk.Meta.Model.CameraModel/TreePlatformData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Model.CameraModel
+{
+    public sealed class TreePlatformData
+    {
+        public IDictionary<string, TreeRevisionData> Revisions { get; set; }
+    }
+}
